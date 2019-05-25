a2f696b5439bde2d1a9844af13569b420747a15f
Ryan Bravo <rrbravo2@gmail.com>
2019-05-25 00:50:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,7 +138,7 @@ end
 def team_colors(team)
  game_hash.each do |teams_playing, info|
    info.each do |attribute, team_data|
-     if attribute == :colors
+     if attribute == team
        return game_hash[teams_playing][:colors]
      end
    end
